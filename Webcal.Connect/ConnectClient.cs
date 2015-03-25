bc555b29986590899059b7edc907799296d99946
Jon Preece <jon@developerhandbook.com>
2015-03-25 11:24:43 +0000

    Localization changes and added Advanced Installer update project

--- Webcal.Connect/ConnectClient.cs
+++ Webcal.Connect/ConnectClient.cs
@@ -1,6 +1,8 @@
 ﻿namespace Webcal.ConnectClient
 {
     using System;
+    using System.Net;
+    using System.Net.Security;
     using System.Security.Cryptography.X509Certificates;
     using System.ServiceModel;
     using System.ServiceModel.Channels;
@@ -21,18 +23,40 @@
 
         public void Open(IConnectKeys connectKeys)
         {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback =
+    ((sender, certificate, chain, sslPolicyErrors) => true);
+
+// trust sender
+System.Net.ServicePointManager.ServerCertificateValidationCallback
+                = ((sender, cert, chain, errors) => cert.Subject.Contains("YourServerName"));
+
+// validate cert by calling a function
+ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateRemoteCertificate);
+
             Binding binding = new ConnectBindingHelper().CreateBinding(new ConnectTokenParameters());
             var serviceAddress = new EndpointAddress(connectKeys.Url);
 
             _channelFactory = new ChannelFactory<IConnectService>(binding, serviceAddress);
 
             var credentials = new ConnectClientCredentials(connectKeys);
-            credentials.ServiceCertificate.SetDefaultCertificate(StoreLocation.LocalMachine, StoreName.My, X509FindType.FindByIssuerName, new Uri(connectKeys.Url).Host);
+            credentials.ServiceCertificate.SetDefaultCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindBySubjectName,"webcalconnect.com");
 
             _channelFactory.Endpoint.Behaviors.Remove(typeof(ClientCredentials));
             _channelFactory.Endpoint.Behaviors.Add(credentials);
 
             Service = _channelFactory.CreateChannel();
+        }
+
+        // callback used to validate the certificate in an SSL conversation
+        private static bool ValidateRemoteCertificate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors policyErrors)
+        {
+            bool result = false;
+            if (cert.Subject.ToUpper().Contains("YourServerName"))
+            {
+                result = true;
+            }
+
+            return result;
         }
 
         public override void ForceClose()
