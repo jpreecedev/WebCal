2fac4c7fb61171bff8a57ab92e6927ec82142920
Jon Preece <jon@developerhandbook.com>
2015-03-29 15:33:05 +0100

    White label by locale - Part 3

---
+++ TachographReader/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿namespace TachographReader
+{
+    using System;
+    using System.Data.Entity;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Threading;
+    using System.Windows;
+    using System.Windows.Threading;
+    using Windows;
+    using Windows.LogInWindow;
+    using DataModel;
+    using DataModel.Core;
+    using DataModel.Library;
+    using DataModel.Migrations;
+    using Library;
+    using Shared;
+    using Shared.Helpers;
+
+    public partial class App
+    {
+        public App()
+        {
+            Dispatcher.CurrentDispatcher.UnhandledException += CurrentDispatcher_UnhandledException;
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            Current.Startup += Current_Startup;
+        }
+
+        private void Current_Startup(object sender, StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen(LocalizationHelper.GetResourceManager().GetString("TXT_SPLASH_SCREEN_PATH"));
+            splashScreen.Show(false);
+
+            if (!InitialiseApplication())
+            {
+                return;
+            }
+
+            splashScreen.Close(new TimeSpan(0, 0, 0));
+
+            CheckForUpdates();
+
+            var window = new LogInWindow();
+            if (window.ShowDialog() == true)
+            {
+                ShutdownMode = ShutdownMode.OnMainWindowClose;
+                MainWindow = new MainWindow();
+
+                MainWindow.ShowDialog();
+            }
+            else
+            {
+                Current.Shutdown();
+            }
+        }
+
+        private static bool InitialiseApplication()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments));
+
+            //Resource dictionaries must be set in code to avoid issues with older operating systems
+            Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("Resources/MainResourceDictionary.xaml", UriKind.Relative) });
+            Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("pack://application:,,,/Fluent;Component/Themes/Generic.xaml") });
+            
+            MigrationHelper.MigrateIfRequired();
+            MigrationHelper.HackMigrationHistoryTable();
+
+            //Prepare database
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TachographContext, Configuration>());
+
+            TachographContext context = new TachographContext();
+            context.Database.CreateIfNotExists();
+
+            //Make database accessible to all users
+            MigrationHelper.SetDatabasePermissions();
+
+            //Seed database
+            ISettingsRepository<WorkshopSettings> generalSettings = SeedDataHelper.SeedDatabase();
+            MigrationHelper.ApplyDataHacks();
+
+            //Back up the database, if needed
+            BackupRestoreManager.BackupIfRequired(generalSettings.GetWorkshopSettings());
+
+            //Apply theme
+            ThemeSettings themeSettings = ContainerBootstrapper.Container.GetInstance<ISettingsRepository<ThemeSettings>>().GetThemeSettings();
+            Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = themeSettings.Source });
+
+            return true;
+        }
+
+        private static void CheckForUpdates()
+        {
+            try
+            {
+                Process.Start("AutoUpdater.exe", "/silentall");
+            }
+            catch
+            {
+            }
+        }
+
+        private static void CurrentDispatcher_UnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBoxHelper.ShowError(string.Format("{0}\n\n{1}", TachographReader.Properties.Resources.EXC_UNHANDLED_EXCEPTION, ExceptionPolicy.HandleException(ContainerBootstrapper.Container, e.Exception)));
+            e.Handled = true;
+        }
+    }
+}
---
+++ TachographReader/Library/MigrationHelper.cs
@@ -0,0 +1,133 @@
+﻿using TachographReader.Properties;
+
+namespace TachographReader.Library
+{
+    using System;
+    using System.Data.SqlServerCe;
+    using System.IO;
+    using System.Linq;
+    using System.Security.AccessControl;
+    using System.Security.Principal;
+    using DataModel;
+    using DataModel.Core;
+    using DataModel.Library;
+    using Shared;
+    using Shared.Helpers;
+
+    public static class MigrationHelper
+    {
+        public static void ApplyDataHacks()
+        {
+            var miscellaneousSettingsRepository = ContainerBootstrapper.Container.GetInstance<ISettingsRepository<MiscellaneousSettings>>();
+            var miscellaneousSettings = miscellaneousSettingsRepository.GetMiscellaneousSettings();
+            var lastMigrationHackId = miscellaneousSettings.LastMigrationHackId;
+
+            if (lastMigrationHackId == 0)
+            {
+                var workshopSettings = ContainerBootstrapper.Container.GetInstance<ISettingsRepository<WorkshopSettings>>().GetWorkshopSettings();
+                var printerSettingsRepository = ContainerBootstrapper.Container.GetInstance<ISettingsRepository<PrinterSettings>>();
+
+                var printerSettings = printerSettingsRepository.GetPrinterSettings();
+                printerSettings.AutoPrintLabels = workshopSettings.AutoPrintLabels;
+
+                printerSettingsRepository.Save(printerSettings);
+                miscellaneousSettings.LastMigrationHackId = 1;
+                miscellaneousSettingsRepository.Save(miscellaneousSettings);
+            }
+        }
+
+        public static void MigrateIfRequired()
+        {
+            MigrateOldPath((string)AppDomain.CurrentDomain.GetData("DataDirectory"));
+            MigrateWorkshopImages();
+        }
+
+        public static void SetDatabasePermissions()
+        {
+            var databasePath = (string)AppDomain.CurrentDomain.GetData("DataDirectory");
+
+            if (IsAdministrator())
+            {
+                GrantAccess(Path.Combine(databasePath, "tacho.sdf"));
+            }
+        }
+
+        private static void MigrateWorkshopImages()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Webcal", "ContactImages");
+            if (Directory.Exists(path))
+            {
+                var supportedFormats = new[] { Resources.TXT_EXTENSION_JPG, Resources.TXT_EXTENSION_JPEG, Resources.TXT_EXTENSION_PNG };
+                var logo = Directory.GetFiles(path).FirstOrDefault(f => supportedFormats.Any(format => string.Equals(Path.GetExtension(f), format, StringComparison.CurrentCultureIgnoreCase)));
+                if (logo != null)
+                {
+                    var repository = ContainerBootstrapper.Container.GetInstance<ISettingsRepository<WorkshopSettings>>();
+                    try
+                    {
+                        var workshopSettings = repository.GetWorkshopSettings();
+                        workshopSettings.Image = ImageHelper.LoadImageSafely(logo);
+                        repository.Save(workshopSettings);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Webcal"));
+                directoryInfo.EmptyFolder();
+                directoryInfo.Delete(true);
+            }
+        }
+
+        private static void MigrateOldPath(string databasePath)
+        {
+            var oldPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (File.Exists(Path.Combine(oldPath, "webcal.sdf")) && !(File.Exists(Path.Combine(databasePath, "webcal.sdf"))))
+            {
+                File.Move(Path.Combine(oldPath, "webcal.sdf"), Path.Combine(databasePath, "tacho.sdf"));
+            }
+        }
+
+        private static bool IsAdministrator()
+        {
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static void GrantAccess(string fullPath)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(fullPath);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            dInfo.SetAccessControl(dSecurity);
+        }
+
+        public static void HackMigrationHistoryTable()
+        {
+            var databasePath = Path.Combine((string)AppDomain.CurrentDomain.GetData("DataDirectory"), "tacho.sdf");
+
+            if (!File.Exists(databasePath))
+            {
+                return;
+            }
+
+            using (SqlCeConnection connection = new SqlCeConnection(string.Format(@"Data Source = {0}", databasePath)))
+            {
+                using (SqlCeCommand cmd = new SqlCeCommand(@"UPDATE [__MigrationHistory] SET ContextKey = 'TachographReader.DataModel.Migrations.Configuration' WHERE ContextKey = 'Webcal.DataModel.Migrations.Configuration'", connection))
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
