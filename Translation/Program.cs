1bf19878687d1fb906fbb7acd2c1c7fbb80c6d80
Jon Preece <jon@developerhandbook.com>
2015-05-17 06:22:39 +0100

    Updated translation to include Russian on CSV file

--- Translation/Program.cs
+++ Translation/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using System.Xml;
     using Properties;
@@ -10,18 +11,21 @@
     {
         private static void Main()
         {
+            var path = "C:\\Source\\TachographReader\\TachographReader.Shared\\Properties\\Resources.resx";
+            var path2 = "C:\\Source\\TachographReader\\TachographReader.Shared\\Properties\\Resources.ru.resx";
+
             Console.WriteLine("Do you want to generate a translation file? (Y/N)");
 
             bool generateTranslationFile = Console.ReadLine().ToUpper() == "Y";
             if (generateTranslationFile)
             {
                 Console.WriteLine("Enter the path of the resources file to translate:");
-                var resourcesPath = Console.ReadLine();
-                if (!string.IsNullOrEmpty(resourcesPath) && File.Exists(resourcesPath))
-                {
-                    GenerateTranslationFile(resourcesPath);
-                    Console.WriteLine("The translation file has been saved to your desktop.");
-                }
+                //var resourcesPath = Console.ReadLine();
+                //if (!string.IsNullOrEmpty(resourcesPath) && File.Exists(resourcesPath))
+                //{
+                GenerateTranslationFile(path, path2);
+                Console.WriteLine("The translation file has been saved to your desktop.");
+                //}
             }
             else
             {
@@ -39,12 +43,16 @@
             Console.ReadLine();
         }
 
-        private static void GenerateTranslationFile(string path)
+        private static void GenerateTranslationFile(string path, string path2)
         {
             var document = new XmlDocument();
             document.Load(path);
 
+            var russianDocument = new XmlDocument();
+            russianDocument.Load(path2);
+
             var dataElements = document.GetElementsByTagName("data");
+            var russianDataElements = russianDocument.GetElementsByTagName("data");
 
             var sb = new StringBuilder();
             sb.AppendLine("Name,English,Translation");
@@ -54,15 +62,31 @@
                 var name = dataElement.GetAttribute("name");
                 var value = dataElement.GetElementsByTagName("value")[0].InnerText;
 
-                sb.AppendLine(string.Format("{0},\"{1}\",", name, value));
+                var translation = FindDataElementValue(russianDataElements, name);
+
+                sb.AppendLine(string.Format("{0},\"{1}\",\"{2}\"", name, value, translation));
             }
 
-            File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), string.Format("{0}.csv", Path.GetFileNameWithoutExtension(path))), sb.ToString());
+            File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), string.Format("{0}.csv", Path.GetFileNameWithoutExtension(path))), sb.ToString(), Encoding.UTF8);
+        }
+
+        private static string FindDataElementValue(XmlNodeList dataElements, string name)
+        {
+            foreach (XmlElement dataElement in dataElements)
+            {
+                var elementName = dataElement.GetAttribute("name");
+                if (elementName == name)
+                {
+                    return dataElement.GetElementsByTagName("value")[0].InnerText;
+                }
+            }
+
+            return string.Empty;
         }
 
         private static void ReadTranslationFile(string path)
         {
-            StringBuilder sb= new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             sb.Append(Resources.TXT_DOCUMENT_HEADER);
             sb.Append(Resources.TXT_DOCUMENT_SCHEMA);
 
