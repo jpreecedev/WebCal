be3068ee229c8219cecbd1b9f8e02132c17e0abc
Jon Preece <jonpreece@hotmail.co.uk>
2016-01-07 15:33:38 +0000

    Added single instance, print plaque from web page

--- TachographReader/App.xaml.cs
+++ TachographReader/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace TachographReader
+﻿using System.Collections.Generic;
+using Microsoft.Shell;
+using TachographReader.Windows.ReprintWindow;
+
+namespace TachographReader
 {
     using System;
     using System.Data.Entity;
@@ -19,14 +23,61 @@
     using Shared;
     using Shared.Helpers;
 
-    public partial class App
+    public partial class App : ISingleInstanceApp
     {
         public App()
         {
-            Dispatcher.CurrentDispatcher.UnhandledException += CurrentDispatcher_UnhandledException;
-            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            if (SingleInstance<App>.InitializeAsFirstInstance("TachographReader"))
+            {
+                Dispatcher.CurrentDispatcher.UnhandledException += CurrentDispatcher_UnhandledException;
+                ShutdownMode = ShutdownMode.OnExplicitShutdown;
 
-            Current.Startup += Current_Startup;
+                Current.Startup += Current_Startup;
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            if (args == null || args.Count < 2)
+            {
+                return true;
+            }
+
+            try
+            {
+                var arg = args[1];
+                var protocolSplit = arg.Split(':');
+                var parameters = protocolSplit[1];
+                var parametersSplit = parameters.Split(',');
+                var command = parametersSplit[0];
+                var parameter = parametersSplit[1];
+
+                switch (command)
+                {
+                    case "reprintlabel":
+
+                        var window = new ReprintWindow
+                        {
+                            DataContext = new ReprintWindowViewModel
+                            {
+                                ReprintMode = ReprintMode.Label,
+                                RegistrationNumber = parameter
+                            }
+                        };
+
+                        var dataContext = (ReprintWindowViewModel) window.DataContext;
+                        dataContext.ReprintCommand.Execute(window);
+
+                        break;
+                }
+
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return true;
         }
 
         private async void Current_Startup(object sender, StartupEventArgs e)
@@ -94,6 +145,9 @@
             //Apply theme
             ThemeSettings themeSettings = ContainerBootstrapper.Resolve<ISettingsRepository<ThemeSettings>>().GetThemeSettings();
             Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = themeSettings.Source });
+
+            //Launch Webcal Protocol
+            RegistryHelper.RegisterProtocol();
 
             //Initialise worker queue, which will start checking for unprocessed tasks
             WorkerHelper.Initialize();
---
+++ TachographReader/Library/RegistryHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace TachographReader.Library
+{
+    public static class RegistryHelper
+    {
+        public static void RegisterProtocol()
+        {
+            var subkey = Registry.ClassesRoot.CreateSubKey("wcconnect");
+            subkey.SetValue("", "URL:Webcal Connect Protocol");
+            subkey.SetValue("URL Protocol", "");
+
+            var shell = subkey.CreateSubKey("shell");
+            shell.SetValue("", "open");
+            var open = shell.CreateSubKey("open");
+            open.SetValue("", "");
+            var command = open.CreateSubKey("command");
+
+            command.SetValue("", "\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"" + " " + "\"" + "%1" + "\"" + " " + "\"" + "%2" + "\"" + " " + "\"" + "%3" + "\"");
+        }
+    }
+}
